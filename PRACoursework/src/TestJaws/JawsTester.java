656f7f3da172f2c78f43e9e994819ece96716d74
Benjamin Krarup <k1502440@kcl.ac.uk>
2016-02-23 14:22:53 +0000

    Testing Jaws object

--- PRACoursework/src/TestJaws/JawsTester.java
+++ PRACoursework/src/TestJaws/JawsTester.java
@@ -3,6 +3,7 @@ package TestJaws;
 import api.jaws.Jaws;
 
 public class JawsTester {
+	
 	public static void main(String[] args){
 		Jaws testJaws = new Jaws("jphHPbni3MIBmMKu", "jbB8OPuNG5Sxw11c");
 		
