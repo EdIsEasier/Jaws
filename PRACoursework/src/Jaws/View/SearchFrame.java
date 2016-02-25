b1005f1bfcd33ee87fd06d8606ef5a0853fec4b8
Benjamin Krarup <k1502440@kcl.ac.uk>
2016-02-25 13:30:11 +0000

    Adding more to the search frame

--- PRACoursework/src/Jaws/View/SearchFrame.java
+++ PRACoursework/src/Jaws/View/SearchFrame.java
@@ -1,8 +1,13 @@
 package Jaws.View;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -23,14 +28,39 @@ public class SearchFrame extends JFrame{
 		JPanel jpTrackerStage = new JPanel(new GridLayout (2, 1));
 		JPanel jpTrackerTag = new JPanel(new GridLayout(2, 1));
 		JPanel jpTrackerSearch = new JPanel(new BorderLayout());
+		
+		jpTrackerRange.setBorder(BorderFactory.createLineBorder(Color.black));
+		jpTrackerGender.setBorder(BorderFactory.createLineBorder(Color.black));
+		jpTrackerStage.setBorder(BorderFactory.createLineBorder(Color.black));
+		jpTrackerTag.setBorder(BorderFactory.createLineBorder(Color.black));
+		jpTrackerSearch.setBorder(BorderFactory.createLineBorder(Color.black));
+		
 		add(jpTracker, BorderLayout.WEST);
 		jpTracker.add(jpTrackerRange);
-		jpTracker.add(jpTrackerRange);
+		jpTracker.add(jpTrackerGender);
 		jpTracker.add(jpTrackerStage);
 		jpTracker.add(jpTrackerTag);
 		jpTracker.add(jpTrackerSearch);
 		
 		jpTrackerRange.add(new JLabel("Tracking Range"));
 		jpTrackerGender.add(new JLabel("Gender"));
+		jpTrackerStage.add(new JLabel("Stage of Life"));
+		jpTrackerTag.add(new JLabel("Tag Location"));
+		
+		JComboBox cbRange = new JComboBox();
+		JComboBox cbGender = new JComboBox();
+		JComboBox cbStage = new JComboBox();
+		JComboBox cbTag = new JComboBox();
+		
+		jpTrackerRange.add(cbRange);
+		jpTrackerGender.add(cbGender);
+		jpTrackerStage.add(cbStage);
+		jpTrackerTag.add(cbTag);
+		
+		JButton jbSearch = new JButton();
+		jpTrackerSearch.add(jbSearch, BorderLayout.NORTH);
+		jpTrackerSearch.add(new JLabel(new ImageIcon(this.getClass().getResource("resources/randypic.jpg"))), BorderLayout.CENTER);
+		
+		pack();
 	}
 }
