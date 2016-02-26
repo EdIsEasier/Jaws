9f6609c18945b0fc2c360be98ab372913ef67b1d
tomas <tomas@192.168.1.9>
2016-02-26 11:20:05 +0000

    Favourites Class

---
+++ PRACoursework/src/testingjaw/testJaw.java
@@ -0,0 +1,10 @@
+package testingjaw;
+import api.jaws.Jaws;
+
+public class testJaw {
+
+	public static void main(String[] args){
+		Jaws tomas = new Jaws("jphHPbni3MIBmMKu", "jbB8OPuNG5Sxw11c");
+		System.out.println(tomas.getLastUpdated());
+}
+}
