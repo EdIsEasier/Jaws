daec6046faf85f9928417469f9a4c4dc8cf5a681
Edvinas <edvinas.kruglovas@kcl.ac.uk>
2016-02-26 12:28:59 +0000

    Updated the SearchListener class with three new methods: filterByRange, filterByGender and filterByStage. These methods should help filter the search results according to what the user selects in the combo boxes.

--- PRACoursework/src/Jaws/Controller/SearchListener.java
+++ PRACoursework/src/Jaws/Controller/SearchListener.java
@@ -33,28 +33,103 @@ public class SearchListener implements ActionListener
 
 	private void updateAllSharks()
 	{
-		ArrayList<String> sharkNames = jaws.getSharkNames();
-		for (String s : sharkNames)
+		// ArrayList<String> sharkNames = jaws.getSharkNames();
+		for (String s : jaws.getSharkNames())
 		{
 			allSharks.add(jaws.getShark(s));
+		}
+	}
+
+	private void filterByRange(List<Shark> sharks, String range)
+	{
+		ArrayList<Ping> neededSharks;
+		switch(range)
+		{
+			case "All":
+				sharks.addAll(allSharks);
+				break;
+			case "Last 24 hours":
+				neededSharks = jaws.past24Hours();
+				for (Ping p : neededSharks)
+					sharks.add(jaws.getShark(p.getName()));
+				break;
+			case "Last Week":
+				neededSharks = jaws.pastWeek();
+				for (Ping p : neededSharks)
+					sharks.add(jaws.getShark(p.getName()));
+				break;
+			case "Last Month":
+				neededSharks = jaws.pastMonth();
+				for (Ping p : neededSharks)
+					sharks.add(jaws.getShark(p.getName()));
+				break;
+		}
+	}
+
+	private void filterByGender(List<Shark> sharks, String gender)
+	{
+		switch(gender)
+		{
+			case "All":
+				break;
+			case "Male":
+				for (Shark s : sharks)
+				{
+					if (s.getGender().equals("Female"))
+						sharks.remove(s);
+				}
+				break;
+			case "Female":
+				for (Shark s : sharks)
+				{
+					if (s.getGender().equals("Male"))
+						sharks.remove(s);
+				}
+				break;
+		}
+	}
+
+	private void filterByStage(List<Shark> sharks, String stage)
+	{
+		switch(stage)
+		{
+			case "All":
+				break;
+			case "Mature":
+				for (Shark s : sharks)
+				{
+					if (!s.getStageOfLife().equals("Mature"))
+					{
+						sharks.remove(s);
+					}
+				}
+				break;
+			case "Immature":
+				for (Shark s : sharks)
+				{
+					if (!s.getStageOfLife().equals("Immature"))
+					{
+						sharks.remove(s);
+					}
+				}
+				break;
+			case "Undetermined":
+				for (Shark s : sharks)
+				{
+					if (!s.getStageOfLife().equals("Undetermined"))
+					{
+						sharks.remove(s);
+					}
+				}
+				break;
 		}
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e)
 	{
-		ArrayList<Ping> neededSharks = new ArrayList<>();
-		switch(range)
-		{
-			case "Last 24 hours":
-				neededSharks = jaws.past24Hours();
-				break;
-			case "Last Week":
-				neededSharks = jaws.pastWeek();
-				break;
-			case "Last Month":
-				neededSharks = jaws.pastMonth();
-				break;
-		}
+		filterByRange(foundSharks, range);
+		filterByGender(foundSharks, gender);
+		filterByStage(foundSharks, stage);
 	}
 }
