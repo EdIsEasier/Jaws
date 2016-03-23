c23af731a655d0e666e148daff5eabbeb77b80e9
tomas <tomas@10.40.134.26>
2016-03-23 18:31:26 +0000

    setting up the API for pie charts

---
+++ PRACoursework/src/Jaws/View/Statistics.java
@@ -0,0 +1,44 @@
+package Jaws.View;
+
+import javax.swing.JFrame;
+
+public class  Statistics extends JFrame {
+	
+	
+	public Statistics(){
+		super ("Statistics");
+		this.setSize(1500, 500);
+		this.setDefaultCloseOperation(HIDE_ON_CLOSE);
+		widgetscreation();
+		this.setVisible(true);
+		
+		
+	}
+	
+	private void widgetscreation() {
+		
+		
+	}
+
+	
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
